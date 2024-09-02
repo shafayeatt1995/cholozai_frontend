b5ed2cc3b544c44b476e17dfdd5a4fa20c602bfb
shafayeatt1995 <shafayetalanik@gmail.com>
2024-09-02 21:00:44 +0600

    Division post added

--- components/Nav/index.vue
+++ components/Nav/index.vue
@@ -14,10 +14,12 @@
           >
           <div class="relative text-left" v-click-outside="() => (show = '')">
             <button
-              class="flex px-5 py-2 text-sm font-medium text-gray-800 dark:text-white hover:text-blue-500 gap-2 items-center"
+              class="flex px-5 py-2 text-sm font-medium dark:text-white hover:text-blue-500 gap-2 items-center"
               type="button"
               :class="
-                show === 'travel' ? 'text-blue-500' : 'dark:text-gray-400'
+                show === 'travel'
+                  ? 'text-blue-500'
+                  : 'text-gray-800 dark:text-gray-400'
               "
               @click="show = show === 'travel' ? '' : 'travel'"
             >
@@ -30,15 +32,14 @@
                 v-if="show === 'travel'"
               >
                 <div class="px-2 py-1">
-                  <div
-                    v-for="division in divisions"
-                    :key="division._id"
-                    class=""
-                  >
-                    <a
+                  <div v-for="division in divisions" :key="division.name">
+                    <nuxt-link
                       class="flex items-center space-x-2 px-5 py-2 text-sm lg:space-x-4 text-gray-700 hover:text-blue-500 focus:text-blue-500 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-900 rounded-md transition-all duration-200"
-                      href="#"
-                      ><span> {{ division.name }}</span></a
+                      :to="{
+                        name: 'division-name',
+                        params: { name: division.slug },
+                      }"
+                      ><span> {{ division.name }}</span></nuxt-link
                     >
                   </div>
                 </div>
---
+++ pages/division/_name.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container mx-auto">
+    <div
+      class="grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3"
+      v-if="posts && posts.length"
+    >
+      <BlogSinglePost
+        v-for="(post, key) in posts"
+        :key="key + 'i'"
+        :post="post"
+      />
+    </div>
+    <div v-else class="w-full flex flex-col justify-center items-center">
+      <div ref="empty" class="h-96"></div>
+      <h1 class="text-3xl font-bold">No Blog post found</h1>
+    </div>
+    <div
+      class="mt-10 flex items-center justify-center"
+      v-if="posts && posts.length"
+    >
+      <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm">
+        <button
+          v-if="pageNumber == 1"
+          disabled
+          class="relative flex items-center gap-1 rounded-l-md border border-gray-300 bg-white px-3 py-2 pr-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
+        >
+          <i class="fa-solid fa-angle-left text-xs"></i>
+          <span>Previous</span>
+        </button>
+        <nuxt-link
+          v-else
+          :to="{ name: 'blog-page-page', params: { page: pageNumber - 1 } }"
+          class="relative flex items-center gap-1 rounded-l-md border border-gray-300 bg-white px-3 py-2 pr-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
+        >
+          <i class="fa-solid fa-angle-left text-xs"></i>
+          <span>Previous</span>
+        </nuxt-link>
+        <nuxt-link
+          v-if="posts && posts.length === 18"
+          :to="{ name: 'blog-page-page', params: { page: pageNumber + 1 } }"
+          class="relative flex items-center gap-1 rounded-r-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
+        >
+          <span>Next</span>
+          <i class="fa-solid fa-angle-right text-xs"></i>
+        </nuxt-link>
+        <button
+          v-else
+          disabled
+          class="relative flex items-center gap-1 rounded-r-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
+        >
+          <span>Next</span>
+          <i class="fa-solid fa-angle-right text-xs"></i>
+        </button>
+      </nav>
+    </div>
+    <div v-else class="flex justify-center items-center"></div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { initLottie } from "@/utils";
+export default {
+  name: "BlogPage",
+  async asyncData(context) {
+    try {
+      const { store, error, route } = context;
+      const { params } = route;
+      const { apiUrl } = store.getters;
+      const name = params?.name;
+      let res = await axios.get(`${apiUrl}/fetch/posts-division/${name}`);
+      if (res.data) {
+        const { posts } = res.data;
+        return { posts };
+      } else {
+        error({ statusCode: 404, message: "Not found" });
+      }
+    } catch (err) {
+      console.log(err);
+      const { error } = context;
+      error({
+        statusCode: 500,
+        message: "Something wrong. Please try again 2",
+      });
+    }
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  computed: {
+    pageNumber() {
+      return +this.$route.params.page;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      if (this.$refs.empty) {
+        initLottie(this.$refs.empty, "/lottie/empty.json");
+      }
+    });
+  },
+};
+</script>
