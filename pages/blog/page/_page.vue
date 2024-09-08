55d9f10d340c14a1c0dedc2257b11ac4ba91417b
shafayeatt1995 <shafayetalanik@gmail.com>
2024-09-08 12:43:46 +0600

    Images name updated

--- pages/blog/page/_page.vue
+++ pages/blog/page/_page.vue
@@ -18,40 +18,7 @@
       class="mt-10 flex items-center justify-center"
       v-if="posts && posts.length"
     >
-      <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm">
-        <button
-          v-if="pageNumber == 1"
-          disabled
-          class="relative flex items-center gap-1 rounded-l-md border border-gray-300 bg-white px-3 py-2 pr-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <i class="fa-solid fa-angle-left text-xs"></i>
-          <span>Previous</span>
-        </button>
-        <nuxt-link
-          v-else
-          :to="{ name: 'blog-page-page', params: { page: pageNumber - 1 } }"
-          class="relative flex items-center gap-1 rounded-l-md border border-gray-300 bg-white px-3 py-2 pr-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <i class="fa-solid fa-angle-left text-xs"></i>
-          <span>Previous</span>
-        </nuxt-link>
-        <nuxt-link
-          v-if="posts && posts.length === 18"
-          :to="{ name: 'blog-page-page', params: { page: pageNumber + 1 } }"
-          class="relative flex items-center gap-1 rounded-r-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <span>Next</span>
-          <i class="fa-solid fa-angle-right text-xs"></i>
-        </nuxt-link>
-        <button
-          v-else
-          disabled
-          class="relative flex items-center gap-1 rounded-r-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <span>Next</span>
-          <i class="fa-solid fa-angle-right text-xs"></i>
-        </button>
-      </nav>
+      <Paginate :posts="posts" />
     </div>
     <div v-else class="flex justify-center items-center"></div>
   </div>
